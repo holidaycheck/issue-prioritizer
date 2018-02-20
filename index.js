059cd2aeab30b5be09f8f0454c63dc2d26b9f452
HeeL <parizhskiy@gmail.com>
2018-02-20 22:47:10 +0100

    Listen to more issue events: labeled, unlabeled and opened

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const issueLabelsHandler = require('./src/issueLabelsHandler');
 const addLabel = () => {};
 const calculatePriority = () => {};
 
-module.exports = robot => robot.on('issues.label', issueLabelsHandler.bind(null, addLabel, calculatePriority));
+module.exports = (robot) => {
+    const events = ['issues.labeled', 'issues.unlabeled', 'issues.opened'];
+
+    return robot.on(events, issueLabelsHandler.bind(null, addLabel, calculatePriority));
+};
--- test/unit/indexSpec.js
+++ test/unit/indexSpec.js
@@ -2,11 +2,12 @@ const index = require('../../index');
 
 describe('index', () => {
     it('handles issue labels events', () => {
+        const expectedEvents = ['issues.labeled', 'issues.unlabeled', 'issues.opened'];
         const robot = { on: jest.fn() };
         index(robot);
 
         expect(robot.on).toHaveBeenCalledTimes(1);
         const firstArgument = robot.on.mock.calls[0][0];
-        expect(firstArgument).toEqual('issues.label');
+        expect(firstArgument).toEqual(expectedEvents);
     });
 });
