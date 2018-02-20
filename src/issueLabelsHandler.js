bce4ae71fe693a2a547565121444aa77b4ae5b53
HeeL <parizhskiy@gmail.com>
2018-02-20 23:52:55 +0100

    Prevent adding label when any metric is missing

--- src/issueLabelsHandler.js
+++ src/issueLabelsHandler.js
@@ -1,3 +1,25 @@
+const R = require('ramda');
+
+const hasMetric = metric =>
+    R.any(R.propSatisfies(
+        R.contains(`${metric}: `),
+        'name'
+    ));
+
+const hasAllMetrics = R.allPass([
+    hasMetric('Potential'),
+    hasMetric('Effort'),
+    hasMetric('Weight')
+]);
+
 module.exports = (context, addLabel, calculatePriority) => {
-    addLabel(context, calculatePriority(context));
+    const addLabelWithPriority = R.compose(
+        addLabel.bind(null, context),
+        calculatePriority.bind(null, context)
+    );
+
+    R.when(
+        hasAllMetrics,
+        addLabelWithPriority
+    )(context.labels);
 };
--- test/unit/src/issueLabelsHandlerSpec.js
+++ test/unit/src/issueLabelsHandlerSpec.js
@@ -16,4 +16,65 @@ describe('issueLabelsHandler', () => {
         expect(addLabelStub).toHaveBeenCalledTimes(1);
         expect(addLabelStub).toHaveBeenCalledWith(context, 'foo-bar');
     });
+
+    it('WILL NOT add priority label when potential label is missing', () => {
+        const context = {
+            labels: [
+                { id: 208045946, name: 'Effort: 0.1' },
+                { id: 123123123, name: 'xxxxxxx: 0.2' },
+                { id: 123456789, name: 'Weight: 0.3' }
+            ]
+        };
+        const addLabelStub = jest.fn();
+        const calculatePriorityStub = jest.fn();
+        issueLabelsHandler(context, addLabelStub, calculatePriorityStub);
+
+        expect(calculatePriorityStub).toHaveBeenCalledTimes(0);
+        expect(addLabelStub).toHaveBeenCalledTimes(0);
+    });
+
+    it('WILL NOT add priority label when weight label is missing', () => {
+        const context = {
+            labels: [
+                { id: 208045946, name: 'Effort: 0.1' },
+                { id: 123123123, name: 'Potential: 0.2' }
+            ]
+        };
+        const addLabelStub = jest.fn();
+        const calculatePriorityStub = jest.fn();
+        issueLabelsHandler(context, addLabelStub, calculatePriorityStub);
+
+        expect(calculatePriorityStub).toHaveBeenCalledTimes(0);
+        expect(addLabelStub).toHaveBeenCalledTimes(0);
+    });
+
+    it('WILL NOT add priority label when effort label is missing', () => {
+        const context = {
+            labels: [
+                { id: 208045946, name: 'Weight: 0.1' },
+                { id: 123123123, name: 'Potential: 0.2' }
+            ]
+        };
+        const addLabelStub = jest.fn();
+        const calculatePriorityStub = jest.fn();
+        issueLabelsHandler(context, addLabelStub, calculatePriorityStub);
+
+        expect(calculatePriorityStub).toHaveBeenCalledTimes(0);
+        expect(addLabelStub).toHaveBeenCalledTimes(0);
+    });
+
+    it('WILL NOT add priority label when all metric labels are missing', () => {
+        const context = {
+            labels: [
+                { id: 208045946, name: 'zzz: 0.1' },
+                { id: 123123123, name: 'foo: 0.2' }
+            ]
+        };
+        const addLabelStub = jest.fn();
+        const calculatePriorityStub = jest.fn();
+        issueLabelsHandler(context, addLabelStub, calculatePriorityStub);
+
+        expect(calculatePriorityStub).toHaveBeenCalledTimes(0);
+        expect(addLabelStub).toHaveBeenCalledTimes(0);
+    });
 });
