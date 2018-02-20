2f398584a6472e6b093b59ecfa7c0913c918c234
HeeL <parizhskiy@gmail.com>
2018-02-20 16:05:22 +0100

    Switch to 4 spaces

--- test/unit/indexSpec.js
+++ test/unit/indexSpec.js
@@ -1,10 +1,12 @@
 const index = require('../../index');
 const issueLabelsHandler = require('../../src/issueLabelsHandler');
 
-it('handles issue labels events', () => {
-  const robot = { on: jest.fn() };
-  index(robot);
+describe('index', () => {
+    it('handles issue labels events', () => {
+        const robot = { on: jest.fn() };
+        index(robot);
 
-  expect(robot.on).toHaveBeenCalledTimes(1);
-  expect(robot.on).toHaveBeenCalledWith('issues.label', issueLabelsHandler);
+        expect(robot.on).toHaveBeenCalledTimes(1);
+        expect(robot.on).toHaveBeenCalledWith('issues.label', issueLabelsHandler);
+    });
 });
