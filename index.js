b8e2dce69c23c6709ba760183fba1dc7198856b3
HeeL <parizhskiy@gmail.com>
2018-02-21 18:00:19 +0100

    Inject addLabel function in index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const issueLabelsHandler = require('./src/issueLabelsHandler');
 const calculatePriority = require('./src/calculatePriority');
-
-const addLabel = () => {};
+const addLabel = require('./src/addLabel');
 
 module.exports = (robot) => {
     const events = ['issues.labeled', 'issues.unlabeled', 'issues.opened'];
