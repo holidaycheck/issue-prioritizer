18e952b721160eb49f700e76c6098fda7e045f43
HeeL <parizhskiy@gmail.com>
2018-02-22 23:50:31 +0100

    Use ava with sinon for addLabelSpec

--- test/unit/src/addLabelSpec.js
+++ test/unit/src/addLabelSpec.js
@@ -1,35 +1,35 @@
-const addLabel = require('../../../src/addLabel');
+import test from 'ava';
+import sinon from 'sinon';
+import addLabel from '../../../src/addLabel';
 
-describe('addLabel', () => {
-    const issueNumber = 123;
-    const owner = 'foo bar owner';
-    const repo = 'foobaria repo';
-    const labelName = 'some labelzzz';
+const issueNumber = 123;
+const owner = 'foo bar owner';
+const repo = 'foobaria repo';
+const labelName = 'some labelzzz';
 
-    const createContext = (addLabels) => {
-        const issue = { number: issueNumber };
-        const repository = {
-            name: repo,
-            owner: { login: owner }
-        };
-
-        return {
-            payload: { issue, repository },
-            github: { issues: { addLabels } }
-        };
+const createContext = (addLabels) => {
+    const issue = { number: issueNumber };
+    const repository = {
+        name: repo,
+        owner: { login: owner }
     };
 
-    it('calls addLabel with correct issue object', () => {
-        const addLabelsStub = jest.fn();
-        const context = createContext(addLabelsStub);
-        addLabel(context, labelName);
+    return {
+        payload: { issue, repository },
+        github: { issues: { addLabels } }
+    };
+};
 
-        expect(addLabelsStub).toHaveBeenCalledTimes(1);
-        expect(addLabelsStub).toHaveBeenCalledWith({
-            number: issueNumber,
-            labels: [labelName],
-            owner,
-            repo
-        });
-    });
+test('calls addLabel with correct issue object', t => {
+    const addLabelsStub = sinon.stub();
+    const context = createContext(addLabelsStub);
+    addLabel(context, labelName);
+
+    t.is(addLabelsStub.callCount, 1);
+    t.true(addLabelsStub.calledWith({
+        number: issueNumber,
+        labels: [labelName],
+        owner,
+        repo
+    }));
 });
