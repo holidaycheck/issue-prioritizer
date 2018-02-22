68516d28d82b2ccc27010974e85933189446f343
HeeL <parizhskiy@gmail.com>
2018-02-22 23:59:51 +0100

    Use ava with sinon for issueLabelsHandlerSpec

--- test/unit/src/issueLabelsHandlerSpec.js
+++ test/unit/src/issueLabelsHandlerSpec.js
@@ -1,111 +1,111 @@
-const issueLabelsHandler = require('../../../src/issueLabelsHandler');
+import test from 'ava';
+import sinon from 'sinon';
+import issueLabelsHandler from '../../../src/issueLabelsHandler';
 
-const expectDoNotCalculateOrAddPriorityLabel = (labels) => {
+const expectDoNotCalculateOrAddPriorityLabel = (t, labels) => {
     const context = {
         payload: {
             issue: { labels }
         }
     };
-    const addLabelStub = jest.fn();
-    const calculatePriorityStub = jest.fn();
+    const addLabelStub = sinon.stub();
+    const calculatePriorityStub = sinon.stub();
     issueLabelsHandler(addLabelStub, calculatePriorityStub, context);
 
-    expect(calculatePriorityStub).toHaveBeenCalledTimes(0);
-    expect(addLabelStub).toHaveBeenCalledTimes(0);
+    t.is(calculatePriorityStub.callCount, 0);
+    t.is(addLabelStub.callCount, 0);
 };
 
-describe('issueLabelsHandler', () => {
-    it('calculates priority label when all metrics specified', () => {
-        const context = {
-            payload: {
-                issue: {
-                    labels: [
-                        { id: 208045946, name: 'Effort: 0.3' },
-                        { id: 123123123, name: 'Potential: 0.3' },
-                        { id: 123456789, name: 'Weight: 0.5' },
-                        { id: 123456789, name: 'Foobaria: zzz' }
-                    ]
-                }
+test('calculates priority label when all metrics specified', t => {
+    const context = {
+        payload: {
+            issue: {
+                labels: [
+                    { id: 208045946, name: 'Effort: 0.3' },
+                    { id: 123123123, name: 'Potential: 0.3' },
+                    { id: 123456789, name: 'Weight: 0.5' },
+                    { id: 123456789, name: 'Foobaria: zzz' }
+                ]
             }
-        };
-        const expectedLabelsForCalculation = {
-            effort: 0.3,
-            potential: 0.3,
-            weight: 0.5
-        };
-        const addLabelStub = jest.fn();
-        const calculatePriorityStub = jest.fn().mockReturnValue('1.0');
-        issueLabelsHandler(addLabelStub, calculatePriorityStub, context);
+        }
+    };
+    const expectedLabelsForCalculation = {
+        effort: 0.3,
+        potential: 0.3,
+        weight: 0.5
+    };
+    const addLabelStub = sinon.stub();
+    const calculatePriorityStub = sinon.stub().returns('1.0');
+    issueLabelsHandler(addLabelStub, calculatePriorityStub, context);
 
-        expect(calculatePriorityStub).toHaveBeenCalledTimes(1);
-        expect(calculatePriorityStub).toHaveBeenCalledWith(expectedLabelsForCalculation);
-    });
+    t.is(calculatePriorityStub.callCount, 1);
+    t.true(calculatePriorityStub.calledWith(expectedLabelsForCalculation));
+});
 
-    it('adds calculated priority label when all metrics specified', () => {
-        const context = {
-            payload: {
-                issue: {
-                    labels: [
-                        { id: 208045946, name: 'Effort: 0.1' },
-                        { id: 123123123, name: 'Potential: 0.2' },
-                        { id: 123456789, name: 'Weight: 0.3' }
-                    ]
-                }
+test('adds calculated priority label when all metrics specified', t => {
+    const context = {
+        payload: {
+            issue: {
+                labels: [
+                    { id: 208045946, name: 'Effort: 0.1' },
+                    { id: 123123123, name: 'Potential: 0.2' },
+                    { id: 123456789, name: 'Weight: 0.3' }
+                ]
             }
-        };
-        const addLabelStub = jest.fn();
-        const calculatePriorityStub = jest.fn().mockReturnValue('foo-bar');
-        issueLabelsHandler(addLabelStub, calculatePriorityStub, context);
+        }
+    };
+    const addLabelStub = sinon.stub();
+    const calculatePriorityStub = sinon.stub().returns('foo-bar');
+    issueLabelsHandler(addLabelStub, calculatePriorityStub, context);
 
-        expect(addLabelStub).toHaveBeenCalledTimes(1);
-        expect(addLabelStub).toHaveBeenCalledWith(context, 'Priority: foo-bar');
-    });
+    t.is(addLabelStub.callCount, 1);
+    t.true(addLabelStub.calledWith(context, 'Priority: foo-bar'));
+});
 
-    it('WILL NOT add priority label when potential label is missing', () => {
-        const labels = [
-            { id: 208045946, name: 'Effort: 0.1' },
-            { id: 123123123, name: 'xxxxxxx: 0.2' },
-            { id: 123456789, name: 'Weight: 0.3' }
-        ];
+test('WILL NOT add priority label when potential label is missing', t => {
+    const labels = [
+        { id: 208045946, name: 'Effort: 0.1' },
+        { id: 123123123, name: 'xxxxxxx: 0.2' },
+        { id: 123456789, name: 'Weight: 0.3' }
+    ];
 
-        expectDoNotCalculateOrAddPriorityLabel(labels);
-    });
+    expectDoNotCalculateOrAddPriorityLabel(t, labels);
+});
 
-    it('WILL NOT add priority label when weight label is missing', () => {
-        const labels = [
-            { id: 208045946, name: 'Effort: 0.1' },
-            { id: 123123123, name: 'Potential: 0.2' }
-        ];
+test('WILL NOT add priority label when weight label is missing', t => {
+    const labels = [
+        { id: 208045946, name: 'Effort: 0.1' },
+        { id: 123123123, name: 'Potential: 0.2' }
+    ];
 
-        expectDoNotCalculateOrAddPriorityLabel(labels);
-    });
+    expectDoNotCalculateOrAddPriorityLabel(t, labels);
+});
 
-    it('WILL NOT add priority label when effort label is missing', () => {
-        const labels = [
-            { id: 208045946, name: 'Weight: 0.1' },
-            { id: 123123123, name: 'Potential: 0.2' }
-        ];
+test('WILL NOT add priority label when effort label is missing', t => {
+    const labels = [
+        { id: 208045946, name: 'Weight: 0.1' },
+        { id: 123123123, name: 'Potential: 0.2' }
+    ];
 
-        expectDoNotCalculateOrAddPriorityLabel(labels);
-    });
+    expectDoNotCalculateOrAddPriorityLabel(t, labels);
+});
 
-    it('WILL NOT add priority label when all metric labels are missing', () => {
-        const labels = [
-            { id: 208045946, name: 'zzz: 0.1' },
-            { id: 123123123, name: 'foo: 0.2' }
-        ];
+test('WILL NOT add priority label when all metric labels are missing', t => {
+    const labels = [
+        { id: 208045946, name: 'zzz: 0.1' },
+        { id: 123123123, name: 'foo: 0.2' }
+    ];
 
-        expectDoNotCalculateOrAddPriorityLabel(labels);
-    });
+    expectDoNotCalculateOrAddPriorityLabel(t, labels);
+});
 
-    it('WILL NOT add priority label when it has been already set', () => {
-        const labels = [
-            { id: 208045946, name: 'Effort: 0.1' },
-            { id: 123123123, name: 'Potential: 0.2' },
-            { id: 123456789, name: 'Weight: 0.3' },
-            { id: 123456789, name: 'Priority: 0.5' }
-        ];
+test('WILL NOT add priority label when it has been already set', t => {
+    const labels = [
+        { id: 208045946, name: 'Effort: 0.1' },
+        { id: 123123123, name: 'Potential: 0.2' },
+        { id: 123456789, name: 'Weight: 0.3' },
+        { id: 123456789, name: 'Priority: 0.5' }
+    ];
 
-        expectDoNotCalculateOrAddPriorityLabel(labels);
-    });
+    expectDoNotCalculateOrAddPriorityLabel(t, labels);
 });
