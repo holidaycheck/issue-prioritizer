174ae79fccba501b6ecca27ab87e1c04ae2108fb
HeeL <parizhskiy@gmail.com>
2018-02-22 23:03:32 +0100

    Turn off arrow-parens rule to make ava look nicer

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": [ "airbnb-base", "plugin:ramda/recommended" ],
   "rules": {
     "comma-dangle": 0,
+    "arrow-parens": 0,
     "indent": ["error", 4]
   },
   "plugins": [
